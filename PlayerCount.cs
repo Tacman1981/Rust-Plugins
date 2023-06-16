179f4da8b12ef9c69d3d89057ba1c82a723dc7c5
Tacman1981 <101606905+Tacman1981@users.noreply.github.com>
2023-06-16 23:10:01 +0100

    Add files via upload

--- PlayerCount.cs
+++ PlayerCount.cs
@@ -1,13 +1,47 @@
-﻿using Oxide.Core;
+using Oxide.Core;
 using Rust;
 using Oxide.Core.Libraries.Covalence;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace Oxide.Plugins
 {
     [Info("Player Count", "Tacman", "1.0.0")]
     public class PlayerCount : CovalencePlugin
     {
+        private Dictionary<string, float> playerLastCommandTimes = new Dictionary<string, float>();
+        private PluginConfig config;
+
+        private class PluginConfig
+        {
+            public int DelaySeconds { get; set; }
+        }
+
+        protected override void LoadDefaultConfig()
+        {
+            Config.WriteObject(GetDefaultConfig(), true);
+        }
+
+        private PluginConfig GetDefaultConfig()
+        {
+            return new PluginConfig
+            {
+                DelaySeconds = 10 // Default delay of 10 seconds
+            };
+        }
+
+        protected override void LoadConfig()
+        {
+            base.LoadConfig();
+            config = Config.ReadObject<PluginConfig>() ?? GetDefaultConfig();
+            SaveConfig();
+        }
+
+        protected override void SaveConfig()
+        {
+            Config.WriteObject(config, true);
+        }
+
         protected override void LoadDefaultMessages()
         {
             lang.RegisterMessages(new Dictionary<string, string>
@@ -53,9 +87,29 @@ namespace Oxide.Plugins
 
         private string GetMessage(string key, string userId = null) => lang.GetMessage(key, this, userId);
 
+        private bool CanExecuteCommand(IPlayer player)
+        {
+            float lastCommandTime;
+            if (playerLastCommandTimes.TryGetValue(player.Id, out lastCommandTime))
+            {
+                float currentTime = Time.realtimeSinceStartup;
+                int commandDelay = config.DelaySeconds;
+
+                if (currentTime - lastCommandTime < commandDelay)
+                player.Reply("You can not use this command more than once every 60 seconds");
+                    return false;
+            }
+
+            playerLastCommandTimes[player.Id] = Time.realtimeSinceStartup;
+            return true;
+        }
+
         [Command("pop")]
         private void PopCommand(IPlayer player, string command, string[] args)
         {
+            if (!CanExecuteCommand(player))
+                return;
+
             int playerCount = BasePlayer.activePlayerList.Count;
 
             string playerCountText = string.Format(GetMessage("PlayerCountFormat"), playerCount);
