0ca7556d7873badab584656fa94e3352246694cf
Tacman1981 <101606905+Tacman1981@users.noreply.github.com>
2023-06-16 13:56:07 +0100

    Add files via upload

---
+++ PlayerCount.cs
@@ -0,0 +1,72 @@
+﻿using Oxide.Core;
+using Rust;
+using Oxide.Core.Libraries.Covalence;
+using System.Collections.Generic;
+
+namespace Oxide.Plugins
+{
+    [Info("Player Count", "Tacman", "1.0.0")]
+    public class PlayerCount : CovalencePlugin
+    {
+        protected override void LoadDefaultMessages()
+        {
+            lang.RegisterMessages(new Dictionary<string, string>
+            {
+                { "PlayerCountFormat", "[Player Count] Online Players: {0}" },
+            }, this);
+
+            lang.RegisterMessages(new Dictionary<string, string>
+            {
+                { "PlayerCountFormat", "[Player Count] Joueurs en ligne : {0}" },
+            }, this, "fr");
+
+            lang.RegisterMessages(new Dictionary<string, string>
+            {
+                { "PlayerCountFormat", "[Player Count] Online-Spieler: {0}" },
+            }, this, "de");
+
+            lang.RegisterMessages(new Dictionary<string, string>
+            {
+                { "PlayerCountFormat", "[Player Count] Jugadores en línea: {0}" },
+            }, this, "es");
+
+            lang.RegisterMessages(new Dictionary<string, string>
+            {
+                { "PlayerCountFormat", "[Player Count] Giocatori online: {0}" },
+            }, this, "it");
+
+            lang.RegisterMessages(new Dictionary<string, string>
+            {
+                { "PlayerCountFormat", "[Player Count] Онлайн игроки: {0}" },
+            }, this, "ru");
+
+            lang.RegisterMessages(new Dictionary<string, string>
+            {
+                { "PlayerCountFormat", "[Player Count] 在线玩家数：{0}" },
+            }, this, "zh-CN");
+
+            lang.RegisterMessages(new Dictionary<string, string>
+            {
+                { "PlayerCountFormat", "[Player Count] اللاعبين المتصلين: {0}" },
+            }, this, "ar");
+        }
+
+        private string GetMessage(string key, string userId = null) => lang.GetMessage(key, this, userId);
+
+        [Command("pop")]
+        private void PopCommand(IPlayer player, string command, string[] args)
+        {
+            int playerCount = BasePlayer.activePlayerList.Count;
+
+            string playerCountText = string.Format(GetMessage("PlayerCountFormat"), playerCount);
+
+            player.Message(playerCountText);
+
+            foreach (var onlinePlayer in players.Connected)
+            {
+                if (onlinePlayer.Id != player.Id)
+                    onlinePlayer.Message(playerCountText);
+            }
+        }
+    }
+}
---
+++ RocketDamageReduction.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Newtonsoft.Json;
+using Oxide.Core;
+using Oxide.Core.Configuration;
+using Rust;
+
+namespace Oxide.Plugins
+{
+    [Info("RocketDamageReduction", "YourName", "1.0.0")]
+    [Description("Reduces rocket damage to other players")]
+
+    class RocketDamageReduction : RustPlugin
+    {
+        private DynamicConfigFile config;
+        private PluginConfig pluginConfig;
+
+        private void Loaded()
+        {
+            LoadDefaultConfig();
+        }
+
+        protected override void LoadDefaultConfig()
+        {
+            pluginConfig = new PluginConfig
+            {
+                ReductionPercentage = 50f // Default reduction percentage is 50%
+            };
+
+            SaveConfig();
+        }
+
+        protected override void LoadConfig()
+        {
+            base.LoadConfig();
+
+            try
+            {
+                pluginConfig = Config.ReadObject<PluginConfig>();
+            }
+            catch (Exception ex)
+            {
+                PrintWarning($"Failed to load config file: {ex.Message}");
+                LoadDefaultConfig();
+            }
+
+            SaveConfig();
+        }
+
+        protected override void SaveConfig()
+        {
+            Config.WriteObject(pluginConfig);
+        }
+
+        private void OnEntityTakeDamage(BaseCombatEntity entity, HitInfo hitInfo)
+        {
+            // Check if the damaged entity is a player and the attacker is a rocket
+            if (entity is BasePlayer && hitInfo?.Initiator is BaseProjectile && hitInfo?.WeaponPrefab?.name == "rocket_launcher")
+            {
+                BasePlayer damagedPlayer = entity as BasePlayer;
+                BasePlayer attacker = hitInfo.Initiator as BasePlayer;
+
+                // Check if the damaged player is the same as the attacker
+                if (damagedPlayer == attacker)
+                    return;
+
+                float originalDamage = hitInfo.damageTypes.Total();
+                float reductionPercentage = pluginConfig.ReductionPercentage / 100f;
+                float reducedDamage = originalDamage * reductionPercentage;
+
+                hitInfo.damageTypes.ScaleAll(reducedDamage / originalDamage);
+            }
+        }
+
+        private class PluginConfig
+        {
+            [JsonProperty(PropertyName = "Reduction Percentage")]
+            public float ReductionPercentage { get; set; }
+        }
+    }
+}
