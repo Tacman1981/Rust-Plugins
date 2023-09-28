99d820a1cb098d535ef644cd9790125aa462baee
Tacman1981 <101606905+Tacman1981@users.noreply.github.com>
2023-09-28 06:44:02 +0100

    Add files via upload

--- CommandToInvite.cs
+++ CommandToInvite.cs
@@ -1,113 +1,107 @@
-using Oxide.Core;
-using Rust;
-using Oxide.Core.Libraries.Covalence;
+﻿using Oxide.Core;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Oxide.Plugins
 {
-  [Info("Player Count", "Tacman", "1.2.0")]
-  public class PlayerCount : CovalencePlugin
-  {
-    private Dictionary<string, float> playerLastCommandTimes = new Dictionary<string, float>();
-
-    private class PluginConfig
+        [Info("Command To Invite", "Tacman", "1.5.0")]
+        [Description("Allows players to send team invites to other players")]
+    class CommandToInvite : RustPlugin
     {
-      public int DelaySeconds { get; set; }
+        private void SendInvite(BasePlayer sender, BasePlayer target)
+        {
+            RelationshipManager.PlayerTeam playerTeam = sender.Team;
+            //Create a team if the sender is not already part of 1
+            if (playerTeam == null)
+            {
+                if (!TryCreateTeam(sender))
+                {
+                    sender.ChatMessage("Failed to create team.");
+                    return;
+                }
+
+                playerTeam = sender.Team;
+            }
+            //Check if team is full
+            if (playerTeam.members.Count >= 8)
+            {
+                sender.ChatMessage("Your team is already full.");
+                return;
+            }
+            //Check if sender is team leader
+            if (!playerTeam.GetLeader().Equals(sender))
+            {
+                sender.ChatMessage("You are not the team leader.");
+                return;
+            }
+            //Check for valid players
+            if (target == null)
+            {
+                sender.ChatMessage("Player not found");
+                return;
+            }
+            //Check if player is inviting self
+            if (target == sender)
+                {
+                sender.ChatMessage("You can not invite yourself to team");
+            }
+            //Check if player is already in inviting team
+            RelationshipManager.PlayerTeam targetTeam = target.Team;
+            if (targetTeam != null && targetTeam == playerTeam)
+            {
+                sender.ChatMessage($"{target.displayName} is already in your team.");
+                return;
+            }
+            //Check if player is already in another team
+            if (targetTeam != null)
+            {
+                sender.ChatMessage($"{target.displayName} is already in a team.");
+                return;
+            }
+
+            playerTeam.SendInvite(target);
+
+            sender.ChatMessage($"You have invited {target.displayName} to your team.");
+            target.ChatMessage($"{sender.displayName} has invited you to join their team from afar. Please press tab to accept or decline");
+        }
+
+        private bool TryCreateTeam(BasePlayer player)
+        {
+            if (global::RelationshipManager.maxTeamSize == 0)
+            {
+                player.ChatMessage("Teams are disabled on this server");
+                return false;
+            }
+            if (player.currentTeam != 0UL) { return false; }
+            RelationshipManager.PlayerTeam playerTeam = RelationshipManager.ServerInstance.CreateTeam();
+            playerTeam.teamLeader = player.userID;
+            playerTeam.AddPlayer(player);
+            return true;
+        }
+
+
+        [ChatCommand("invite")]
+        private void InviteCommand(BasePlayer player, string command, string[] args)
+        {
+            if (args.Length != 1)
+            {
+                player.ChatMessage("Usage: /invite <player name or ID>");
+                return;
+            }
+            string playerName = args[0];
+            List<BasePlayer> players = BasePlayer.activePlayerList
+                .Where(x => x.displayName.ToLower().Contains(playerName.ToLower()))
+                .ToList();
+            if (players.Count == 0) { player.ChatMessage("Player not found."); }
+            else if (players.Count > 1)
+            {
+                var distinctPlayers = players.Select(x => x.displayName).Distinct().ToList();
+                if (distinctPlayers.Count > 1) { player.ChatMessage($"Multiple players found. Please refine your search criteria."); return; }
+                SendInvite(player, players.First());
+            }
+            else { SendInvite(player, players[0]); }
+        }
     }
-
-    private const string AdminCountPermission = "playercount.admin"; // New permission constant
-
-    private PluginConfig config;
-
-    protected void Init()
-    {
-      permission.RegisterPermission(AdminCountPermission, this);
-    }
-
-    protected override void LoadDefaultConfig()
-    {
-      Config.WriteObject(GetDefaultConfig(), true);
-    }
-
-    private PluginConfig GetDefaultConfig()
-    {
-      return new PluginConfig
-      {
-        DelaySeconds = 60 // Default delay of 10 seconds
-      };
-    }
-
-    protected override void LoadConfig()
-    {
-      base.LoadConfig();
-      config = Config.ReadObject<PluginConfig>() ?? GetDefaultConfig();
-      SaveConfig();
-    }
-
-    protected override void SaveConfig()
-    {
-      Config.WriteObject(config, true);
-    }
-
-    protected override void LoadDefaultMessages()
-    {
-      lang.RegisterMessages(new Dictionary<string, string>
-      {
-        { "PlayerCountFormat", "Online Players: {0}" },
-        { "AdminCountFormat", "Online Admins: {0}" }
-      }, this, "en");
-
-      // Add Dutch language support
-      lang.RegisterMessages(new Dictionary<string, string>
-      {
-        { "PlayerCountFormat", "Aantal spelers online: {0}" },
-        { "AdminCountFormat", "Aantal admins online: {0}" }
-      }, this, "nl");
-    }
-
-    private string GetMessage(string key, string userId = null) => lang.GetMessage(key, this, userId);
-
-    private bool CanExecuteCommand(IPlayer player)
-    {
-      float currentTime = Time.realtimeSinceStartup;
-      float lastCommandTime;
-
-      if (playerLastCommandTimes.TryGetValue(player.Id, out lastCommandTime))
-      {
-        float commandDelay = config.DelaySeconds;
-
-        if (currentTime - lastCommandTime < commandDelay)
-          return false;
-      }
-
-      playerLastCommandTimes[player.Id] = currentTime;
-      return true;
-    }
-
-    [Command("pop")]
-    private void PopCommand(IPlayer player, string command, string[] args)
-    {
-      if (!CanExecuteCommand(player))
-        return;
-
-      int adminCount = 0;
-      int playerCount = 0;
-
-      foreach (var basePlayer in BasePlayer.activePlayerList)
-      {
-        if (basePlayer.IsAdmin)
-          adminCount++;
-        else
-          playerCount++;
-      }
-
-      string playerCountText = string.Format(GetMessage("PlayerCountFormat", player.Id), playerCount);
-      string adminCountText = player.HasPermission(AdminCountPermission) ? string.Format(GetMessage("AdminCountFormat", player.Id), adminCount) : "";
-
-      string response = $"[Player Count] {playerCountText} {adminCountText}";
-
-      player.Reply(response, null, player.Id);
-    }
-  }
 }
+
