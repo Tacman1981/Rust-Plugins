def48ca0ff409caa152c71ce997f2614f632ab57
Tacman1981 <101606905+Tacman1981@users.noreply.github.com>
2024-06-09 04:01:02 +0100

    Add files via upload

--- BuoyantCrates.cs
+++ BuoyantCrates.cs
@@ -1,1 +1,95 @@
-Blanked due to massive changes to how buoyancy is calculated in game now
+﻿using UnityEngine;
+using Rust;
+using System;
+
+namespace Oxide.Plugins
+{
+    [Info("Buoyant Crates", "Tacman", "1.5.1")]
+    [Description("Makes helicopter and code locked hackable crates buoyant")]
+    class BuoyantCrates : RustPlugin
+    {
+        #region Config
+        public PluginConfig _config;
+
+        public PluginConfig GetDefaultConfig()
+        {
+            return new PluginConfig
+            {
+                DetectionRate = 1,
+            };
+        }
+
+        public class PluginConfig
+        {
+            public int DetectionRate;
+        }
+        #endregion
+
+        #region Oxide
+        protected override void LoadDefaultConfig() => Config.WriteObject(GetDefaultConfig(), true);
+
+        void Init()
+        {
+            _config = Config.ReadObject<PluginConfig>();
+        }
+
+        void OnEntitySpawned(BaseEntity entity)
+        {
+            if (entity == null || (entity.ShortPrefabName != "heli_crate" && entity.ShortPrefabName != "codelockedhackablecrate" && entity.ShortPrefabName != "supply_drop")) return;
+            MakeBuoyant buoyancy = entity.gameObject.AddComponent<MakeBuoyant>();
+            buoyancy.buoyancyScale = 3f;
+            buoyancy.detectionRate = _config.DetectionRate;
+        }
+        #endregion
+
+        #region Classes
+        class MakeBuoyant : MonoBehaviour
+        {
+            public float buoyancyScale;
+            public int detectionRate;
+            private BaseEntity _entity;
+            private SupplyDrop _supplyDrop;
+
+            void Awake()
+            {
+                _entity = GetComponent<BaseEntity>();
+                _supplyDrop = _entity as SupplyDrop;
+                if (_entity == null) Destroy(this);
+            }
+
+            void FixedUpdate()
+            {
+                if (_entity == null)
+                {
+                    Destroy(this);
+                    return;
+                }
+
+                Bounds bounds = new Bounds(_entity.WorldSpaceBounds().ToBounds().center, _entity.WorldSpaceBounds().ToBounds().size);
+
+                if (UnityEngine.Time.frameCount % detectionRate == 0 && WaterLevel.Factor(bounds, true, true, _entity) > 0.65f)
+                {
+                    if (_supplyDrop != null)
+                    {
+                        _supplyDrop.RemoveParachute();
+                        _supplyDrop.isLootable = true;
+                    }
+                    BuoyancyComponent(_entity);
+                    Destroy(this);
+                }
+            }
+
+            void BuoyancyComponent(BaseEntity entity)
+            {
+                Buoyancy buoyancy = entity.gameObject.AddComponent<Buoyancy>();
+                buoyancy.buoyancyScale = buoyancyScale;
+                buoyancy.rigidBody = entity.gameObject.GetComponent<Rigidbody>();
+                buoyancy.rigidBody.velocity = Vector3.zero;
+                buoyancy.rigidBody.angularVelocity = Vector3.zero;
+                buoyancy.rigidBody.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezeRotationX;
+                buoyancy.SavePointData();
+            }
+        }
+        #endregion
+    }
+}
