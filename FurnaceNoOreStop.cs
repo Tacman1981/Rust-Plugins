8e9f068aac11ca465f6dacd7c4b83d7ccd4b019b
Tacman1981 <101606905+Tacman1981@users.noreply.github.com>
2025-03-12 18:09:04 +0000

    Added new file
    Turns off furnaces and refineries, once all ores or crude is finished cooking, regardless if there is wood left. No longer affects lanterns.

---
+++ FurnaceNoOreStop.cs
@@ -0,0 +1,43 @@
+﻿using Oxide.Core;
+using Oxide.Core.Plugins;
+using Oxide.Game.Rust.Cui;
+using UnityEngine;
+
+namespace Oxide.Plugins
+{
+    [Info("FurnaceNoOreStop", "Tacman", "1.0.1")]
+    [Description("Stops furnaces when there are no more ores, retaining the fuel, and ensures lanterns are unaffected.")]
+    public class FurnaceNoOreStop : RustPlugin
+    {
+        private void OnOvenCooked(BaseOven oven)
+        {
+            if (oven == null || oven.inventory == null)
+                return;
+
+            if (oven.ShortPrefabName.Contains("lantern"))
+                return;
+
+            bool hasOres = false;
+
+            foreach (Item item in oven.inventory.itemList)
+            {
+                if (item.info.shortname.Contains("ore") || (item.info.shortname.Contains("crude")))
+                {
+                    hasOres = true;
+                    break;
+                }
+            }
+
+            if (!hasOres)
+            {
+                oven.StopCooking();
+
+                BasePlayer player = BasePlayer.FindByID(oven.OwnerID);
+                if (player != null)
+                {
+                    player.ChatMessage("Your cooking has stopped because there are no more cookables.");
+                }
+            }
+        }
+    }
+}
