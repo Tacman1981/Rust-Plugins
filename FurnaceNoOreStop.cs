69e72ba06e135f0a5785c5844952320ebaf67803
Tacman1981 <101606905+Tacman1981@users.noreply.github.com>
2025-03-15 21:07:00 +0000

    Added permission.

--- FurnaceNoOreStop.cs
+++ FurnaceNoOreStop.cs
@@ -1,27 +1,37 @@
-﻿using Oxide.Core;
+using Oxide.Core;
 using Oxide.Core.Plugins;
 using Oxide.Game.Rust.Cui;
 using UnityEngine;
 
 namespace Oxide.Plugins
 {
-    [Info("FurnaceNoOreStop", "Tacman", "1.0.1")]
+    [Info("FurnaceNoOreStop", "Tacman", "1.0.2")]
     [Description("Stops furnaces when there are no more ores, retaining the fuel, and ensures lanterns are unaffected.")]
     public class FurnaceNoOreStop : RustPlugin
     {
+        private string usePerm = "furnacenoorestop.use";
+
+        void Init()
+        {
+            permission.RegisterPermission(usePerm, this);
+        }
+
         private void OnOvenCooked(BaseOven oven)
         {
             if (oven == null || oven.inventory == null)
                 return;
 
-            if (oven.ShortPrefabName.Contains("lantern"))
+            if (!permission.UserHasPermission(oven.OwnerID.ToString(), usePerm))
+                return;
+
+            if (oven.ShortPrefabName.Contains("lantern") || oven.ShortPrefabName.Contains("bbq") || oven.ShortPrefabName.Contains("fire"))
                 return;
 
             bool hasOres = false;
 
             foreach (Item item in oven.inventory.itemList)
             {
-                if (item.info.shortname.Contains("ore") || (item.info.shortname.Contains("crude")))
+                if (item.info.shortname.Contains("ore") || item.info.shortname.Contains("crude"))
                 {
                     hasOres = true;
                     break;
@@ -36,6 +46,33 @@ namespace Oxide.Plugins
                 if (player != null)
                 {
                     player.ChatMessage("Your cooking has stopped because there are no more cookables.");
+                }
+            }
+        }
+
+        void OnItemAddedToContainer(ItemContainer container, Item item)
+        {
+            if (container?.entityOwner == null || item == null)
+                return;
+
+            BaseOven oven = container.entityOwner as BaseOven;
+            if (oven == null || (!oven.ShortPrefabName.Contains("furnace") && !oven.ShortPrefabName.Contains("refinery")))
+                return;
+
+            if (!permission.UserHasPermission(oven.OwnerID.ToString(), usePerm))
+                return;
+
+            if (item.info.shortname.Contains("crude") || item.info.shortname.Contains("ore"))
+            {
+                if (!oven.IsOn())
+                {
+                    oven.StartCooking();
+
+                    BasePlayer player = BasePlayer.FindByID(oven.OwnerID);
+                    if (player != null)
+                    {
+                        player.ChatMessage("Your cooking has resumed because cookable items were added.");
+                    }
                 }
             }
         }
